LineageOS/android_device_lineage_sepolicy

sepolicy: Initial repo structure

common/sepolicy.mk
@@ -1,0 +1,7 @@
+#
+# This policy configuration will be used by all products that
+# inherit from Lineage
+#
+
+BOARD_SEPOLICY_DIRS += \
+    device/lineage/sepolicy/common

qcom/sepolicy.mk
@@ -1,0 +1,7 @@
+#
+# This policy configuration will be used by all qcom products
+# that inherit from Lineage
+#
+
+BOARD_SEPOLICY_DIRS += \
+    device/lineage/sepolicy/qcom

AOSP-JF-MM/platform_device_jdc_sepolicy

Signed-off-by: Marco Zanin (B--B) <mrczn.bb@gmail.com>

common/sepolicy.mk
@@ -1,0 +1,7 @@
+#
+# This policy configuration will be used by all products that
+# inherit from Lineage
+#
+
+BOARD_SEPOLICY_DIRS += \
+    device/jdc/sepolicy/common

qcom/sepolicy.mk
@@ -1,0 +1,7 @@
+#
+# This policy configuration will be used by all qcom products
+# that inherit from Lineage
+#
+
+BOARD_SEPOLICY_DIRS += \
+    device/jdc/sepolicy/qcom